211ba6741ee62be0ab0d9ef1ea9239ac77f9852a
unknown <G00387867@gmit.ie>
2020-02-19 16:12:25 +0000

    Following up lecture on functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+# Adam Shaat
+# a function to sqaure numbers
+
+def power(x, y):
+  ans = x
+  y = y - 1
+  while y > 0:
+    ans = ans * x
+    y = y - 1  
+  return ans
+  
+def f(x):
+  ans = (100 * power(x, 2) + 10 * power(x, 3)) // 100 
+  ans = ans - (power(x, 3) // 10)
+  return ans
+
+print(f(2))
+
--- pyprimes.py
+++ pyprimes.py
@@ -1,5 +1,17 @@
 # Adam Shaat
 # computing the primes.
+import math
+
+def isprime(i):
+
+  # loop through all values from 2 up to
+  # but not including i.
+  for j in range(2, math.floor(math.sqrt(i))):
+    if i % j == 0:
+      # if it does, i isn't a prime so exit
+      # the loop and indicate it's not prime.  
+      return False
+  return True
 
 # My list of primes - TBD
 p=[]
@@ -7,19 +19,12 @@ p=[]
 # loop through all the numbers we're 
 # checking for primality.
 
-for i in range(2, 100):
-    # Assume i is a prime
-    isprime = True
-    # loop through all values from 2 up to
-    # but not including i.
-    for j in range(2, i):
-      if i % j == 0:
-        # if it does, i isn't a prime so exit
-        # the loop and indicate it's not prime.  
-        isprime = False
-        break
-    # if i is prime, then append to P. 
-    if isprime:
-     p.append(i)
 
+for i in range(2, 100000):
+
+    # if i is prime, then append to P. 
+    if isprime(i):
+     p.append(i)
+     
+# print out our list
 print(p)
