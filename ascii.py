d639e2fbcc50e4414b2d6a9adce29c29acf24068
unknown <G00387867@gmit.ie>
2020-03-09 21:44:40 +0000

    re organizing pands-problems repositry

--- ascii.py
+++
@@ -1,6 +0,0 @@
-# Adam
-# ascii table
-
-for i in range(0, 256):
-    print(f"{i:3} {i:08b} {chr(i)}")
-    
--- read-a-file.py
+++
@@ -1,6 +0,0 @@
-# Adam
-
-with open("myfile.txt", "a") as f:
-    print(f.tell())
-    f.write("\nHello, from the line!")
-    print(f.tell())
